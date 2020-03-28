2ef0b07b126951581e89332a1dfde6ccd30c690b
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-28 07:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -137,12 +137,14 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if (e.which === LEFT_ARROW) {
-     moveDodgerLeft();
+     moveDodgerLeft()
      e.preventDefault()
+     e.stopPropagation()
    }
    else if(e.which === RIGHT_ARROW) {
-     moveDodgerRight();
+     moveDodgerRight()
      e.preventDefault()
+     e.stopPropagation()
    }
 }
 
